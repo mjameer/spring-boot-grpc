bcb3c5646e8ae8df8b99a8f825035bcbe035d41c
Mohiadeen Ameer <mohiadeen.ameer@tp-link.com>
2024-11-19 21:51:18 -0800

    [Update] Java 8 to Java 21

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Spring Boot gRPC Example
+### Spring Boot gRPC Example
 
 This project covers usage of gRPC in a Spring Boot application
 
-## Commands used
+### Testing Commands
 
 ```
   grpcurl --plaintext localhost:9090 list
@@ -13,3 +13,20 @@ This project covers usage of gRPC in a Spring Boot application
 ## Reference
 
 - [Mike's Spring Boot gRPC Starter](https://yidongnan.github.io/grpc-spring-boot-starter/en/server/getting-started.html)
+
+
+
+
+### How do Protocol Buffers Work?
+
+The following diagram shows how you use protocol buffers to work with your data.
+
+
+### Reference
+
+- https://www.youtube.com/watch?v=jG6sYey45G8
+- https://www.youtube.com/watch?v=hVrwuMnCtok&t=274s
+- https://www.youtube.com/watch?v=4SuFtQV8RCk
+- https://www.youtube.com/watch?v=2CWYorTWyGs&t=191s
+- https://manerajona.medium.com/building-microservices-with-spring-boot-and-grpc-b770ff4ff61b 
+
--- greeting-service/src/main/java/com/mj/grpc/service/GreetingServiceImpl.java
+++ greeting-service/src/main/java/com/mj/grpc/service/GreetingServiceImpl.java
@@ -4,18 +4,20 @@ import com.mj.grpc.GreetingRequest;
 import com.mj.grpc.GreetingResponse;
 import com.mj.grpc.GreetingServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@Slf4j
 @GrpcService
 public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
 
     @Override
     public void greeting(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
         String message = request.getMessage();
-        System.out.println("Received Message: " + message);
+        log.info("Received Message: {} ", message);
 
         GreetingResponse greetingResponse = GreetingResponse.newBuilder()
-            .setMessage("Welcome " + message + ". Hello From gRPC. ")
+            .setMessage("Welcome " + message + ". Hello From gRPC Server. ")
             .build();
 
         responseObserver.onNext(greetingResponse);
